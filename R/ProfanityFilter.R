8bdcc444bf323cb40eeda0ddf6c22882cc18ec55
Mark McPherson <mark.mcpherson1@nhs.net>
2022-01-15 21:16:38 +0000

    Initial commit

---
+++ R/ProfanityFilter.R
@@ -0,0 +1,77 @@
+#' @title R6 Class providing a profanity filter to censor text.
+#' @description Uses a trie data structure of profane words to filter profanity
+#' from text.
+#'
+#' @importFrom R6 R6Class
+#' @importFrom rjson fromJSON
+#'
+#' @export
+# Begin Exclude Linting
+ProfanityFilter <- R6::R6Class(
+  # End Exclude Linting
+  "ProfanityFilter",
+  private = list(
+    .profane_trie = NULL,
+    .censor_profane_words = function(text, censor_char) {
+      text <- strsplit(text, split = " ")[[1]]
+      clean_text <- ""
+
+      for (word in text) {
+        curr_word <- ""
+        if (private$.profane_trie$has_prefix(tolower(word))) {
+          curr_word <- strrep(censor_char, nchar(word))
+        } else {
+          curr_word <- word
+        }
+        clean_text <- paste(clean_text, curr_word)
+      }
+
+      substr(clean_text, 2, nchar(clean_text))
+    }
+  ),
+  public = list(
+    #' @description
+    #' Create a new `ProfanityFilter` object.
+    #'
+    #' @param profane_words Unnamed list of words (by default it reads the included
+    #' `profane_words.json` into a list).
+    #' @return A new `ProfanityFilter` object.
+    #'
+    #' @examples
+    #' \dontrun{
+    #'   # use default profanity list
+    #'   pf <- ProfanityFilter$new()
+    #'
+    #'   # use custom profanity list
+    #'   bad_words <- list("feck", "sod", "damn")
+    #'   pf <- ProfanityFilter$new(bad_words)
+    #' }
+    initialize = function(profane_words = rjson::fromJSON(file = system.file(
+                            "extdata", "profane_words.json",
+                            package = "profanityfilter"
+                          ))) {
+      private$.profane_trie <- Trie$new(profane_words)
+    },
+
+    #' @description
+    #' Apply profanity filtering to a piece of text.
+    #'
+    #' @param text The text to filter.
+    #' @param censor_char Character used to replace all characters in censored
+    #' words (default '*').
+    #' @return The original text, filtered of profanity.
+    #'
+    #' @examples
+    #' \dontrun{
+    #'   pf$censor("Don't say feck or bugger", '+')
+    #'   # "Don't say ++++ or ++++++!"
+    #' }
+    censor = function(text, censor_char = "*") {
+      if (!is.character(text)) text <- as.character(text)
+      if (!is.character(censor_char)) text <- as.character(censor_char)
+
+      private$.censor_profane_words(text, censor_char)
+    }
+  ),
+  cloneable = FALSE
+)
---
+++ R/Trie.R
@@ -0,0 +1,69 @@
+#' @title R6 Class providing a trie data structure
+#' @description Trie is implemented as nested unnamed lists, with a special
+#' character string flag denoting end of a word.
+#'
+#' @importFrom R6 R6Class
+#' @export
+# Begin Exclude Linting
+Trie <- R6::R6Class(
+  # End Exclude Linting
+  "Trie",
+  private = list(
+    .root = list(),
+    .end = "",
+    .insert = function(words) {
+      first <- substr(words, 1, 1)
+      rest <- substr(words, 2, nchar(words))
+      zi <- nchar(words) == 0L
+      c(
+        list(private$.end)[any(zi)],
+        lapply(split(rest[!zi], first[!zi]), private$.insert)
+      )
+    }
+  ),
+  public = list(
+    #' @description
+    #' Create a new `Trie` object.
+    #'
+    #' @param words Unnamed list of words.
+    #' @return A new `Trie` object.
+    #'
+    #' @examples
+    #' words <- list("first", "fist", "fjord")
+    #' t <- Trie$new(words)
+    initialize = function(words) {
+      private$.end <- "_end"
+      private$.root <- private$.insert(words)
+    },
+
+    #' @description
+    #' Check if a given word is present in the trie.
+    #'
+    #' @param word String to check for presence in trie.
+    #' @return Either `TRUE` or `FALSE`.
+    #'
+    #' @examples
+    #' t$has_prefix("firstly")
+    #' # TRUE
+    #' t$has_prefix("firs")
+    #' # FALSE
+    #'
+    has_prefix = function(word) {
+      current <- private$.root
+      for (char in strsplit(word, "")[[1]]) {
+        if (is.null(current[[char]])) {
+          if (private$.end %in% current) {
+            return(TRUE)
+          }
+          return(FALSE)
+        }
+        current <- current[[char]]
+      }
+      if (private$.end %in% current) {
+        return(TRUE)
+      }
+      return(FALSE)
+    }
+  ),
+  cloneable = FALSE
+)
