da25875eb36f801f966b5e42f23651b2e000b024
Mark McPherson <mark.mcpherson1@nhs.net>
2022-01-25 08:15:10 +0000

    Add encode_char_cols function

---
+++ R/encode_char_cols.R
@@ -0,0 +1,25 @@
+#' @title Set character encoding in tibble or data.frame
+#' @description Given an encoding, this sets all character columns to it.
+#'
+#' @importFrom dplyr mutate_at
+#'
+#' @param .data A data.frame or tibble.
+#' @param .encoding A string specifying an encoding..
+#'
+#' @return The original data, with character columns set to given encoding.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' data <- data %>%
+#'   encode_char_cols("latin1")
+#' }
+encode_char_cols <- function(.data, .encoding) {
+  .data %>%
+    mutate_if(
+      is.character,
+      .funs = function(x) {
+        return(`Encoding<-`(x, "latin1"))
+      }
+    )
+}
